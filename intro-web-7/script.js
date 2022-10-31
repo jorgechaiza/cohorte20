27f3b9fc65d0af672a480af2d5df127333101174
jorgechaiza <jorgechavezaraiza93@gmail.com>
2022-10-31 17:28:32 -0600

    javascript 31-10

---
+++ intro-web-7/script.js
@@ -0,0 +1,106 @@
+//Comentario de una sola linea
+/* Comentario para 2 lineas o más, igual no importa */
+
+/* console.log("hello world");
+alert("Alerta de chisme")
+
+console.log("Esta es una suma", 5+5); */
+
+
+var edad=29
+var nombre="Jorge"
+var tengomascota=true
+var hacefrio=null
+var haycafe=undefined
+
+console.log(edad);
+console.log(nombre);
+console.log(tengomascota);
+console.log(hacefrio);
+console.log(haycafe);
+
+console.log(typeof(edad));
+console.log(typeof(nombre));
+console.log(typeof(tengomascota));
+console.log(typeof(hacefrio));
+console.log(typeof(haycafe));
+
+var anionacimiento=1993
+var anioactual=2022
+
+var josue= 2000
+var mafer= 1999
+var josesotelo= 1992
+var melissa= 1996
+var oscar= 1995
+
+
+console.log("jorge" , anioactual-anionacimiento);
+console.log("josue" , anioactual-josue);
+console.log("mariaFernanda" , anioactual-mafer);
+console.log("joseSotelo" , anioactual-josesotelo);
+console.log("melissa" , anioactual-melissa);
+console.log("oscar" , anioactual-oscar);
+
+
+console.log("Inicio ejercicio");
+console.log(Number(false));
+console.log(String(false));
+console.log(Boolean(false));
+
+console.log(Number(true));
+console.log(String(true));
+console.log(Boolean(true));
+
+console.log(Number(0));
+console.log(String(0));
+console.log(Boolean(0));
+
+console.log(Number(1));
+console.log(String(1));
+console.log(Boolean(1));
+
+console.log(Number("0"));
+console.log(String("0"));
+console.log(Boolean("0"));
+
+console.log(Number("000"));
+console.log(String("000"));
+console.log(Boolean("000"));
+
+console.log(Number("1"));
+console.log(String("1"));
+console.log(Boolean("1"));
+
+console.log(Number("NaN"));
+console.log(String("NaN"));
+console.log(Boolean("NaN"));
+
+console.log(Number(Infinity));
+console.log(String(Infinity));
+console.log(Boolean(Infinity));
+
+console.log(Number(-Infinity));
+console.log(String(-Infinity));
+console.log(Boolean(-Infinity));
+
+console.log(Number(""));
+console.log(String(""));
+console.log(Boolean(""));
+
+//13
+ console.log(Number("20"));
+ console.log(String("20"));
+ console.log(Boolean("20"));
+//14
+ console.log(Number("veinte"));
+ console.log(String("veinte"));
+ console.log(Boolean("veinte"));
+
+ console.log(Number(null));
+ console.log(String(null));
+ console.log(Boolean(null));
+
+ console.log(Number(undefined));
+ console.log(String(undefined));
+ console.log(Boolean(undefined));
---
+++ js-02/funcionesjs.js
@@ -0,0 +1,112 @@
+let miNombre="jorgeEstebanChavez";
+console.log(miNombre);
+
+function myFunction() {
+    let nombre = "Jorge";
+    console.log(nombre);
+}
+
+myFunction();
+
+function funcion1() {
+    let valor = 2;
+    valor = valor + 3;
+    return valor;
+}
+let resultado= funcion1();
+console.log("el resultado es: " + resultado);
+
+
+function funcion2() {
+    let valor1 = 2;
+    valor1 = valor1 + 3;
+    console.log("el resultado es: " + valor1);  
+}
+funcion2();
+
+
+
+let variableGlobal = 5
+function funcion3() {
+    let variableLocal = "El valor es. "
+    console.log(variableLocal + variableGlobal);
+}
+funcion3();
+
+
+function funcion4(valor2 = 4) {
+    let numero = 6;
+    numero = numero * valor2;
+    return numero;
+}
+let salida = funcion4(2);
+console.log("El producto es: " + salida);
+
+
+//Funciones anónimas, no se les asigna un nombre
+let funcion0 = function(parametro){
+    parametro = parametro * 2;
+    return parametro;
+}
+console.log("Llamando a la funcion anonima: " + funcion0(4));
+
+let funcion01 = function(caracteres){
+    return "Hola " + caracteres;
+}(" Lunes ");
+console.log("El resultado es: " + funcion01);
+
+
+
+//TIPO FLECHA   
+let suma = (x , y) => {return x+y};
+let resultado1 = suma(5 , 3)
+console.log("La suma es: " + resultado1);
+
+
+//EJERCICIO EN CLASE
+function calcularPrecioTotal(precio) {
+    let impuesto = 1.16
+    let gastosEnvio = 10   
+    
+    let precioTotal = (precio*impuesto) + gastosEnvio
+    return precioTotal
+
+}
+let precioTotal = calcularPrecioTotal(23.34)
+console.log("El precio total es: " + precioTotal);
+
+let precioConImpuestos1 = (1 + 16/100) * precioTotal;
+console.log("El precio total con impuestos es: " + precioConImpuestos1);
+
+
+//EJERCICIO EN FLECHA
+let calcularPrecioTotal1 = (precio , impuesto , gastosEnvio) => {return (precio*impuesto) + gastosEnvio}
+
+let precioTotal1 = calcularPrecioTotal1(23.34 , 1.16 , 10)
+console.log("El precio total de nuevo es: " + precioTotal1); 
+
+let precioConImpuestos = (1 + 16/100) * precioTotal1;
+
+console.log("El precio total con impuestos es: " + precioConImpuestos);
+
+//EJERCICIO FUNCION ANONIMA
+let funcion02 = function (precio = 23.34) {
+    let impuesto = 1.16
+    let gastosEnvio = 10
+
+    return (precio*impuesto) + gastosEnvio
+    let precioTotal = funcion02
+
+}
+console.log("en anonimo el resultado es: " + precioTotal);
+
+
+
+let miObjeto = {
+    nombre: "Juan",
+    edad: 30
+};
+
+let mensaje = "tu nombre es: " + miObjeto.nombre;
+mensaje += " tienes " + miObjeto["edad"] + " años";
+console.log(mensaje);
